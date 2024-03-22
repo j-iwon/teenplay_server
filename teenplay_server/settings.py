ce0f9e2706dca969678f690e35bf039fb52031ad
jiwon <zznu0521@gmail.com>
2024-03-22 09:19:41 +0900

    nick

--- teenplay_server/settings.py
+++ teenplay_server/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ae#4x$6v8%w$ltg(5hdq43$*=+1_v@451gj$2j&t++x9#9o01h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','43.201.97.254']
 
 
 # Application definition
@@ -188,6 +188,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # 파일 접근 시
 MEDIA_URL = '/upload/'
